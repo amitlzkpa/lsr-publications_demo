97bbab092bc9fc1bc80aba3c8f4a6c8577ec567b
amitlzkpa <amit.lzkpa@gmail.com>
2020-09-25 03:38:05 -0500

    put seadragon viewer in own component

---
+++ src/components/SeaDragonViewer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div id="openseadragon1" style="width: 800px; height: 600px;"></div>
+  </div>
+</template>
+
+<script>
+import OpenSeadragon from 'openseadragon';
+
+
+export default {
+  async mounted() {
+    let viewer = OpenSeadragon({
+        id: "openseadragon1",
+        prefixUrl: "/openseadragon/images/",
+        tileSources: "/imgs/out/output.dzi"
+    });
+  }
+}
+</script>
