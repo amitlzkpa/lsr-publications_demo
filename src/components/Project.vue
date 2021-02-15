7cc6e56468d658cd3903c3085097102f0abcae57
amitlzkpa <amit.lzkpa@gmail.com>
2021-02-15 14:23:51 -0500

    show sheet details as fetched from backend

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -33,14 +33,11 @@
         <SeaDragonViewer />
         <div>
           <p class="p-text-light">{{ project.title }}</p>
-          <h2>Site Analysis</h2>
-          <hr />
-          <p>
-            Landscape, extreme climate, and isolation are the key elements shaping this monastic community. These elements were not only important in determining the geometry of the structure but also played an important role in defining the use of materials.
-          </p>
-          <p>
-            High amounts of snow and rainfall made it essential to use materials like Stone and Mud. Since pure mud is not suitable for such harsh climatic conditions, the main foundation of the complex is made of decreasing sizes of stone and the walls are then built with adobe blocks. This gradient allows for random voids which absorbs any tremors by constantly settling, in order to achieve stability. The undressed stones along with the inward gradient in the foundation helps in added stability by maintaining an angle of repose. This angle of repose in relation to the scale of the foundations allows for an added space in the lower section of the houses as a basement level. The natural feature that has helped this construction withstand even more, is the shallow reach of the hard strata. This helps in minimizing vibrations by acting as a shock absorber. This complex amalgamation of the natural terrain and construction technique,  gives a visual appearance as if they were carved out if the cliff face.
-          </p>
+          <div v-if="project.sheets && project.sheets.length > 0">
+            <h2>{{ project.sheets[0].title }}</h2>
+            <hr />
+            <p>{{ project.sheets[0].content }}</p>
+          </div>
         </div>
       </div>
     </div>
